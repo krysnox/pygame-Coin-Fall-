5335f0a4a58b286501b28e107c0a0287e579676a
krsynox <sainikartikct25@gmail.com>
2024-11-01 20:24:30 +0530

    fixed difficulty in cart.py and game_screen.py

--- src/game_screens/game_screen.py
+++ src/game_screens/game_screen.py
@@ -4,6 +4,7 @@ import math
 from pygame.locals import QUIT, KEYUP
 from src.game_screens.screen import Screen
 from src.misc.game_enums import Game_mode
+from src.misc.game_enums import Difficulty
 
 from src.objects.cart import Cart
 from src.objects.coin import Coin
@@ -59,7 +60,7 @@ class Game_screen(Screen):
         c = self.get_random_entity(self.i, self.res, self.size, self.surface)
         self.coinlist.append(c)
 
-        for b in self.coinlist[0:self.i:self.game_manager.difficulty.value["DENSITY"]]:                                   
+        for b in self.coinlist[0:self.i:self.game_manager.difficulty["DENSITY"]]:                                   
             # (use 14 or 15) this is for the rate at which
             # objects fall, can change this
             b.draw()
--- src/objects/cart.py
+++ src/objects/cart.py
@@ -36,12 +36,12 @@ class Cart(object):
                 try:
                     if not coin.collected:
                         if coin.type == Entity.BLUE_COIN:
-                            self.points += 3*self.game_manager.difficulty.value["SCORE_MULTIPLIER"]
+                            self.points += 3*self.game_manager.difficulty["SCORE_MULTIPLIER"]
                         elif coin.type == Entity.BOMB:
                             pygame.time.delay(500)
                             self.dead = True  # Replace quit with death
                         else:
-                            self.points += 1*self.game_manager.difficulty.value["SCORE_MULTIPLIER"]
+                            self.points += 1*self.game_manager.difficulty["SCORE_MULTIPLIER"]
 
                         coin.collect()
                 except AttributeError:
